e92945f09c190aea0f6c97927844c645afb8667f
Hendro <hendrowunga073@gmail.com>
2024-09-19 14:14:13 +0700

    contrroller

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,6 +3,7 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
@@ -20,8 +21,8 @@ public class PageController {
     // about route
 
     @RequestMapping("/about")
-    public String aboutPage(Model model){
-        model.addAttribute("isLogin",true);
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", true);
         System.out.println("About page loading");
         return "about";
     }
@@ -33,4 +34,23 @@ public class PageController {
         System.out.println("services page loading");
         return "services";
     }
+
+    // contact page
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+
+    // login
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    // register
+    @GetMapping("/register")
+    public String register() {
+        return new String("register");
+    }
+
 }
