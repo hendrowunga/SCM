4d1991b5e2a3a0f38d5f07a8124cb56dd6b1d681
Hendro <hendrowunga073@gmail.com>
2024-09-22 12:07:39 +0700

    forms

---
+++ src/main/java/com/scm/forms/UserForm.java
@@ -0,0 +1,29 @@
+package com.scm.forms;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class UserForm {
+    @NotBlank(message = "Username is required")
+    @Size(min = 3,message = "Min 3 Characters is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid Email Address")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6,message = "Min 6 Character is required")
+    private String password;
+
+    @NotBlank(message = "About is required")
+    private String about;
+    @Size(min = 8,max = 12,message = "Invalid Phone Number")
+    private String phoneNumber;
+}
