5bc5d70cd6cf91ec8fa20f34c76be16bedd6dd2c
Hendro <hendrowunga073@gmail.com>
2024-09-22 13:30:11 +0700

    implementation

--- src/main/java/com/scm/services/impl/UserServiceImpl.java
+++ src/main/java/com/scm/services/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.scm.services.impl;
 
 import com.scm.entities.User;
+import com.scm.helpers.ResourceNotFoundException;
 import com.scm.repositories.UserRepo;
 import com.scm.services.EmailService;
 import com.scm.services.UserService;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -22,10 +24,17 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private EmailService emailService;
 
-    private Logger logger= LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public User saveUser(User user) {
+        String userId= UUID.randomUUID().toString();
+        user.setUserId(userId);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        // set the user role
+
         return userRepo.save(user);
     }
 
@@ -36,31 +45,49 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Optional<User> updateUser(User user) {
-        return Optional.empty();
+        User user2 = userRepo.findById(user.getUserId()).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        user2.setName(user.getName());
+        user2.setEmail(user.getEmail());
+        user2.setPassword(user.getPassword());
+        user2.setAbout(user.getAbout());
+        user2.setPhoneNumber(user.getPhoneNumber());
+        user2.setProfilePic(user.getProfilePic());
+        user2.setEnabled(user.isEnabled());
+        user2.setEmailVerified(user.isEmailVerified());
+        user2.setPhoneVerified(user.isPhoneVerified());
+        user2.setProvider(user.getProvider());
+        user2.setProviderUserId(user.getProviderUserId());
+
+        // save the user in database
+        User save = userRepo.save(user2);
+        return Optional.ofNullable(save);
     }
 
     @Override
     public void deleteUser(String id) {
-
+        User user2 = userRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("User Not Found"));
+        userRepo.delete(user2);
     }
 
     @Override
     public boolean isUserExist(String userId) {
-        return false;
+        User user2 = userRepo.findById(userId).orElse(null);
+        return user2 != null ? true : false;
     }
 
     @Override
     public boolean isUserExistByEmail(String email) {
-        return false;
+        User user = userRepo.findByEmail(email).orElse(null);
+        return user != null ? true : false;
     }
 
     @Override
     public List<User> getAllUsers() {
-        return List.of();
+        return userRepo.findAll();
     }
 
     @Override
     public User getUserByEmail(String email) {
-        return null;
+        return userRepo.findByEmail(email).orElse(null);
     }
 }
