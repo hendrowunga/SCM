5c9f5c77f237c391d628615f46158db752b9a5e3
Hendro <hendrowunga073@gmail.com>
2024-09-23 20:54:34 +0700

    repository

--- src/main/java/com/scm/repositories/UserRepo.java
+++ src/main/java/com/scm/repositories/UserRepo.java
@@ -14,8 +14,8 @@ public interface UserRepo extends JpaRepository<User, String> {
 
     Optional<User> findByEmail(String email);
 
-    Optional<User> findByEmailAndPassword(String email, String password);
-
-    Optional<User> findByEmailToken(String id);
+//    Optional<User> findByEmailAndPassword(String email, String password);
+//
+//    Optional<User> findByEmailToken(String id);
 
 }
