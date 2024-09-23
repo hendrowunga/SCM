f45df04660802cc4595884fb3335e9cbae56b76f
Hendro <hendrowunga073@gmail.com>
2024-09-24 01:47:27 +0700

    config

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -1,22 +1,45 @@
 package com.scm.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    //    User create and login using java code with in memory service
+    // user create and login using java code with in memory service
+
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withUsername("admin").password("admin123").build();
 
-        var inMemoryUserDetailsManger = new InMemoryUserDetailsManager();
-        return inMemoryUserDetailsManger;
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("admin123")
+                .password("admin123")
+                .roles("ADMIN", "USER")
+                .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("user123")
+                .password("password")
+                // .roles(null)
+                .build();
+
+        var inMemoryUserDetailsManager = new InMemoryUserDetailsManager(user1,
+                user2);
+        return inMemoryUserDetailsManager;
+
     }
 
 }
