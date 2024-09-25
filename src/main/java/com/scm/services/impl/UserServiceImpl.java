11e06aade2542bc6dfd5e0dc5c6b80f9cff2c8b3
Hendro <hendrowunga073@gmail.com>
2024-09-25 20:54:17 +0700

    service

--- src/main/java/com/scm/services/impl/UserServiceImpl.java
+++ src/main/java/com/scm/services/impl/UserServiceImpl.java
@@ -1,12 +1,14 @@
 package com.scm.services.impl;
 
 import com.scm.entities.User;
+import com.scm.helpers.AppConstants;
 import com.scm.helpers.ResourceNotFoundException;
 import com.scm.repositories.UserRepo;
 import com.scm.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,17 +20,21 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public User saveUser(User user) {
-        String userId= UUID.randomUUID().toString();
+        String userId = UUID.randomUUID().toString();
         user.setUserId(userId);
-
-//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        // password encode
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         // set the user role
-
+        user.setRoleList(List.of(AppConstants.ROLE_USER));
+        logger.info(user.getProvider().toString());
         return userRepo.save(user);
     }
 
