19de2ea61ae2323c9741a3417f43491118d10357
Hendro <hendrowunga073@gmail.com>
2024-09-26 01:15:37 +0700

    config

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.scm.config;
 
 import com.scm.services.impl.SecurityCustomUserDetailService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +11,15 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @Configuration
 public class SecurityConfig {
@@ -28,19 +37,50 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         // configuration
         // Urls dikonfigurasi agar mengetahui mana yang akan dipublikasikan dan mana yang akan diprivasi
         httpSecurity.authorizeHttpRequests(
-                authorization->{
+                authorization -> {
 //                    authorization.requestMatchers("/home","/register","services").permitAll();
-                      authorization.requestMatchers("/user/**").authenticated();
-                      authorization.anyRequest()
-                              .permitAll();
-        });
+                    authorization.requestMatchers("/user/**").authenticated();
+                    authorization.anyRequest()
+                            .permitAll();
+                });
 //        Ini Membuat perubahan pada form login, maka kw modifikasi harus dilakukan di area kode ini.
 
-        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.formLogin
+                (formLogin -> {
+                    formLogin.loginPage("/login");
+                    formLogin.loginProcessingUrl("/authenticate");
+                    formLogin.successForwardUrl("/user/profile");
+
+                    formLogin.failureForwardUrl("/login?error=true");
+//                    formLogin.defaultSuccessUrl("/home");
+
+                    formLogin.usernameParameter("email");
+                    formLogin.passwordParameter("password");
+
+//                    formLogin.failureHandler(new AuthenticationFailureHandler() {
+//                        @Override
+//                        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+//
+//                        }
+//                    });
+//                    formLogin.successHandler(new AuthenticationSuccessHandler() {
+//
+//                        @Override
+//                        public void onAuthenticationSuccess(HttpServletRequest request,
+//                                                            HttpServletResponse response,
+//                                                            Authentication authentication) throws IOException, ServletException {
+//                            // TODO Auto-generated method stub
+//                            throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'");
+//                        }
+//                    });
+
+                    formLogin.failureHandler(null);
+
+                });
         return httpSecurity.build();
     }
 
