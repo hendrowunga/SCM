cdc5e8ed3dfff048f109b894edb9e11e5bf26053
Hendro <hendrowunga073@gmail.com>
2024-09-29 23:12:27 +0700

    "helpers"

--- src/main/java/com/scm/helpers/Helper.java
+++ src/main/java/com/scm/helpers/Helper.java
@@ -1,15 +1,39 @@
 package com.scm.helpers;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Component;
 
 import java.security.Principal;
 
+@Component
 public class Helper {
-    public static String getEmailOfLoggedInUser(Principal principal) {
+    public static String getEmailOfLoggedInUser(Authentication authentication) {
 
+        Principal principal = (Principal) authentication.getPrincipal();
         /* cara untuk mendapatkan email pengguna setelah pengguna berhasil login dengan menggunakan email dan password. */
+        if (principal instanceof OAuth2AuthenticationToken) {
+            var aOAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+            var clientId = aOAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
+
+            if (clientId.equalsIgnoreCase("google")) {
+                System.out.println("Getting email from google");
+
+                // sign with google
+            } else if (clientId.equalsIgnoreCase("github")) {
+                System.out.println("Getting email from github");
+
+                // sign with github
+            }
+            return "";
+
+            // sign with facebook
+        } else {
+            System.out.println("Getting data from local database");
+            return authentication.getName();
+        }
 
 
-        return "";
     }
+
 }
