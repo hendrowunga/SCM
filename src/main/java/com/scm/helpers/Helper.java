3b2cf4ab8dc10f41b4430ab923784ea940ad8bd5
Hendro <hendrowunga073@gmail.com>
2024-09-30 01:46:01 +0700

    "helper"

--- src/main/java/com/scm/helpers/Helper.java
+++ src/main/java/com/scm/helpers/Helper.java
@@ -2,6 +2,7 @@ package com.scm.helpers;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 import java.security.Principal;
@@ -10,22 +11,26 @@ import java.security.Principal;
 public class Helper {
     public static String getEmailOfLoggedInUser(Authentication authentication) {
 
-        Principal principal = (Principal) authentication.getPrincipal();
         /* cara untuk mendapatkan email pengguna setelah pengguna berhasil login dengan menggunakan email dan password. */
-        if (principal instanceof OAuth2AuthenticationToken) {
+        if (authentication instanceof OAuth2AuthenticationToken) {
             var aOAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
             var clientId = aOAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
 
+            var oauth2User = (OAuth2User) authentication.getPrincipal();
+            String username = "";
+
+
             if (clientId.equalsIgnoreCase("google")) {
-                System.out.println("Getting email from google");
-
                 // sign with google
+                System.out.println("Getting email from google");
+                username = oauth2User.getAttribute("email").toString();
             } else if (clientId.equalsIgnoreCase("github")) {
-                System.out.println("Getting email from github");
-
                 // sign with github
+                System.out.println("Getting email from github");
+                username = oauth2User.getAttribute("email") != null ? oauth2User.getAttribute("email").toString()
+                        : oauth2User.getAttribute("login").toString() + "@gmail.com";
             }
-            return "";
+            return username;
 
             // sign with facebook
         } else {
