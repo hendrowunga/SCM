654560a9ae3abda7d9b1b077d6e2217e8d01ae28
Hendro <hendrowunga073@gmail.com>
2024-10-02 01:23:56 +0700

    controller

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -1,16 +1,77 @@
 package com.scm.controllers;
 
+import com.scm.entities.Contact;
+import com.scm.entities.User;
+import com.scm.forms.ContactForm;
+import com.scm.helpers.Helper;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
+import com.scm.services.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.naming.CompositeName;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/user/contacts")
 public class ContactController {
+    private Logger logger = org.slf4j.LoggerFactory.getLogger(ContactController.class);
 
+    @Autowired
+    private UserService userService;
     //    add contact page:handler
     @RequestMapping("/add")
-    public String addContactView() {
+    public String addContactView(Model model) {
+        ContactForm contactForm = new ContactForm();
+        contactForm.setFavorite(true);
+        model.addAttribute("contactForm", contactForm);
         return "user/add_contact";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String saveContact(@Valid @ModelAttribute
+                              ContactForm contactForm,
+                              BindingResult result,
+                              Authentication authentication,
+                              HttpSession httpSession) {
+        if (result.hasErrors()){
+            result.getAllErrors().forEach(error ->logger.info(error.toString()));
+            httpSession.setAttribute("message", Message.builder()
+                    .content("Please correct the following errors")
+                    .type(MessageType.red)
+                    .build());
+            return "user/add_contact";
+        }
+        String username= Helper.getEmailOfLoggedInUser(authentication);
+        User user=userService.getUserByEmail(username);
+
+        Contact contact = new Contact();
+        contact.setName(contactForm.getName());
+        contact.setFavorite(contactForm.isFavorite());
+        contact.setEmail(contactForm.getEmail());
+        contact.setPhoneNumber(contactForm.getPhoneNumber());
+        contact.setAddress(contactForm.getAddress());
+        contact.setDescription(contactForm.getDescription());
+        contact.setUser(user);
+        contact.setLinkedInLink(contactForm.getLinkedInLink());
+        contact.setWebsiteLink(contactForm.getWebsiteLink());
+        if (contactForm.getContactImage() != null && !contactForm.getContactImage().isEmpty()){
+            String filename= UUID.randomUUID().toString();
+            String fileURL=imag
+        }
+
+
+
     }
 
 }
