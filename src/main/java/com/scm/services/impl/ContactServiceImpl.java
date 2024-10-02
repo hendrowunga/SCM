10ba145206ecc626d0f03836d13f3b8f5dd61167
Hendro <hendrowunga073@gmail.com>
2024-10-02 22:10:20 +0700

    impl

--- src/main/java/com/scm/services/impl/ContactServiceImpl.java
+++ src/main/java/com/scm/services/impl/ContactServiceImpl.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class ContactServiceImpl implements ContactService {
 
     @Autowired
