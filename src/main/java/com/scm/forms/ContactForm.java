2bcbb7fcc2a491745ec2f1b630e82724d9407757
Hendro <hendrowunga073@gmail.com>
2024-10-02 22:10:04 +0700

    forms

--- src/main/java/com/scm/forms/ContactForm.java
+++ src/main/java/com/scm/forms/ContactForm.java
@@ -1,5 +1,6 @@
 package com.scm.forms;
 
+import com.scm.validators.ValidFile;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -13,22 +14,24 @@ import org.springframework.web.multipart.MultipartFile;
 @NoArgsConstructor
 @ToString
 public class ContactForm {
+    
     @NotBlank(message = "Name is required")
     private String name;
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email address [example@gmail.com]")
     private String email;
     @NotBlank(message = "Phone Number is required")
-    @Pattern(regexp = "^[0-9]{10}$", message = "Invalid Phone Number")
+    @Pattern(regexp = "^[0-9]{10,12}$", message = "Invalid Phone Number")
     private String phoneNumber;
     @NotBlank(message = "Address is required")
     private String address;
+
     private String description;
     private boolean favorite;
     private String websiteLink;
     private String linkedInLink;
 
-    @ValidFile(message="Invalid file")
+    @ValidFile(message = "Invalid file")
     private MultipartFile contactImage;
 
     private String picture;
